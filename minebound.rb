680af85371bdbafb805ddd8044607f38c1428b6b
Casey Kolderup <casey@kolderup.org>
2011-03-26 00:51:09 -0400

    first commit: shell site with accounts
    No actual game logic yet!

--- minebound.rb
+++ minebound.rb
@@ -1,4 +1,3 @@
-require 'rubygems'
 require 'sinatra'
 require 'data_mapper'
 require 'haml'
@@ -6,25 +5,34 @@ require 'sass'
 require 'rack'
 require 'rack-flash'
 
-#use Rack::Session::Cookie
-#use Rack::Flash
-  
-use OmniAuth::Builder do
-  provider :twitter, "CONSUMER_KEY", "CONSUMER_SECRET"
-  provider :facebook, "APP_ID", "APP_SECRET"
-end
+use Rack::Session::Cookie #TODO: make cookie last more than just the session
+use Rack::Flash
 
-def logged_in?
-  current_user != nil
-end
- 
+##### MODELS #####
+
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite://#{Dir.pwd}/minebound.db")
+
 class User
   include DataMapper::Resource
   property :id, Serial
-  property :email, Text
+  property :email, Text, :unique => true, :required => true
   property :passhash, BCryptHash
   property :mc_name, Text
-  property :admin,  Boolean, default => false
+  property :admin,  Boolean, :default => false
+ 
+  belongs_to :game, :required => false
+
+  def password=(pass)
+    @password = pass
+    self.passhash = BCrypt::Password.create(@password, :cost=> 10)
+  end
+
+  def self.authenticate(email, pass)
+    user = User.first(:email => email)
+    return nil if user.nil?
+    return user if BCrypt::Password.new(user.passhash) == pass
+  end
+
 end
 
 class Game
@@ -38,14 +46,44 @@ class Game
  has n, :already_played, 'User'
 end
 
-get '/auth/:provider/callback' do
-  auth = request.env['omniauth.auth']
-  "Thanks, #{auth['user_info']['name']}! Next I'll make it so that this will log you in or prompt you to fill in info!"
-  #TODO: compare to users, if found, send to login
+DataMapper.auto_upgrade!
+
+##### GAMEPLAY #####
+
+get '/pass' do
+  #TODO: get list of tokens they need to pass on, offer to email it to someone but suggest other options
 end
 
+get '/play' do
+  unless logged_in?
+    flash[:error] = "You must log in or create an account."
+    redirect '/login'
+  end
+
+  #TODO: confirm minecraft username
+  #TODO: inform the person of the rules
+  #TODO: tell them to make sure to come back when they die
+end
+
+post '/play' do
+  unless logged_in?
+    flash[:error] = "You must log in or create an account."
+    redirect '/login', 303
+  end
+
+  #TODO: cash in the token (params[:token])
+  #TODO: go to MC server, add username to whitelist.txt
+end
+
+##### USER ACCOUNT MANAGEMENT #####
+
+def logged_in?
+  session[:u_id] != nil
+end
+
+
 get '/logout' do
-  #session[:u_id] = nil
+  session[:u_id] = nil
   flash[:notice] = "You've been logged out."
   redirect '/login'
 end
@@ -55,35 +93,79 @@ get '/login' do
 end
 
 post '/login' do
-  if user = User.authenticate(params[:username], params[:password])
-    #session[:u_id] = user.id
-    redirect '/account'
+  #TODO: make this HTTPS
+  if user = User.authenticate(params[:email], params[:password])
+    session[:u_id] = user.id
+    redirect '/account', 303
   else
-    info[:notice] = "Incorrect username or password."
-    redirect '/login'
+    flash[:error] = "Incorrect username or password."
+    redirect '/login', 303
   end
 end
 
 get '/account' do
   unless logged_in?
-    info[:notice] = "You must be logged in"
+    flash[:error] = "You must be logged in"
     redirect '/login'
   end
 
-  #TODO: user control panel: offer to let them use a game token, store their minecraft username for later, send out/retrieve a game token
+  @u = User.first( :id => session[:u_id] )
+
+  haml :account
 end
 
-get '/play/:token' do
+post '/account' do
+  #TODO: make this HTTPS
   unless logged_in?
-    info[:notice] = "You must log in or create an account."
-    redirect '/login'
+    flash[:error] = "You must be logged in"
+    redirect '/login', 303
   end
 
-  #TODO: cash in the token
+  @u = User.first( :id => session[:u_id] )
+  @u.mc_name = params[:mc_name] unless params[:mc_name].nil?
+  @u.email = params[:email] unless params[:email].nil?
+  
+  unless params[:password].nil?
+    unless params[:password] == params[:password2]
+      flash[:error] = "Passwords did not match. Try again."
+      redirect '/account', 303
+    end
+    @u.password = params[:password]
+  end
+
+  error 400 unless @u.valid?
+  error 503 unless @u.save
+
+  flash[:notice] = "Account details updated."
+  redirect '/account', 303
+end
+
+get '/signup' do
+  haml :signup
 end
 
 post '/signup' do
-  #TODO: create a user
+  #TODO: make this HTTPS
+  unless params[:password].eql? params[:password2]
+    flash[:error] = "Passwords did not match. Try again."
+    redirect '/signup', 303
+  end
+
+  @u = User.new :email => params[:email],
+                :password => params[:password]
+  @u.mc_name = params[:mc_name] unless params[:mc_name].nil?
+
+  unless @u.valid?
+    @u.errors.each do |e|
+      puts e
+    end
+  end
+
+  error 400 unless @u.valid?
+  error 503 unless @u.save
+
+  session[:u_id] = @u.id
+  redirect '/account', 303
 end
 
 get '/confirm/:token' do
@@ -94,10 +176,43 @@ post '/confirm' do
   #TODO: actually confirm the account
 end
 
+##### MISCELLANEOUS FUNCTIONALITY #####
+
 get '/admin' do
-  #TODO: show a list of games/status of those games
+  #TODO: the status of the current game
+  #TODO: option to ban current player and redirect to /pass (generates a game token)
   #TODO: put a form field under completed games to store the download URL
 end
+
 get '/' do
-  "Nothing here yet"
+  haml :index
 end
+
+get '/minebound.css' do
+  headers 'Content-Type' => 'text/css; charset=utf-8'
+  sass :style
+end
+
+##### SPLASH SERVER INTEGRATION #####
+
+post '/api/player/join' do
+  #TODO: secure, token-based auth
+  #TODO: is this even necessary?
+end
+
+post '/api/player/quit' do
+  #TODO: secure, token-based auth
+  #TODO: is this necessary for anything?
+end
+
+post '/api/player/death' do
+  #TODO: secure, token-based auth
+
+  #TODO: scp to MC server, remove current player from whitelist, add current player to banlist
+  #TODO: add current player to past players, nil out current player
+
+  #TODO: check if that's it, if so, run endgame
+
+  #TODO: ...else email the player to direct them to /pass
+end
+
