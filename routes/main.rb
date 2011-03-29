6e1770af922d94977956941e3fa0121ab33defe0
Casey Kolderup <casey@kolderup.org>
2011-03-28 23:41:50 -0400

    stubbing a few functions for game viewing

--- routes/main.rb
+++ routes/main.rb
@@ -1,7 +1,17 @@
 class Minebound < Sinatra::Application
   
-  get '/' do
+  get '/' do #TODO: add list of recently completed games (and status of current game?)
     haml :index
+  end
+
+  ['/games', '/games/:page'].each do |path|
+    get path do #TODO: paged list of games
+    @page = params[:page] || 1
+
+    end
+  end
+
+  get '/game/:id' do #TODO: display list of blog posts for a game and world file download link 
   end
 
   get '/minebound.css' do
