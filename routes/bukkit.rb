3f6477eee1129faa9ffa87adb4d4854b037bfea3
Casey Kolderup <casey@kolderup.org>
2011-03-29 00:10:00 -0400

    started /death logic

--- routes/bukkit.rb
+++ routes/bukkit.rb
@@ -5,19 +5,32 @@ class Minebound < Sinatra::Application
 
   post '/api/player/death' do #TODO: secure, token-based auth
 
-    #TODO: update Game model
-
-    #TODO: POST /death: check if that's it, if so, run endgame
-
-    #TODO: POST /death: email the player to direct them to /pass
+    #TODO: POST /death: update Game model
     
-    #TODO: POST /death: connect to ec2
-   
-    #TODO: POST /death: remove current player from whitelist on ec2
-  
-    #TODO: POST /death: add current player to banlist on ec2
+    @round = Game.current.last_round
+    @round.finished = Time.now
+    @round.save
     
-    #TODO: POST /death: add current player to past players on ec2
+    @u = @round.user
+
+    if (Game.current.rounds.size == 10) then
+      #TODO: POST /death: run endgame
+    else
+      Pony.mail :to => @u.email,
+                :form => 'no-reply@kolderup.org',
+                :subject => 'Minebound death',
+                :body => erb(:died_email)
+      
+      #TODO: POST /death: connect to ec2
+     
+      #TODO: POST /death: remove current player from whitelist on ec2
+    
+      #TODO: POST /death: add current player to banlist on ec2
+      
+      #TODO: POST /death: add current player to past players on ec2
+
+    end
+
 
   end
 end
