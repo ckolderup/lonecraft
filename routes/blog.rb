5f47cf5105e89ddb09917617bb71795b92738c36
Casey Kolderup <casey@kolderup.org>
2011-04-09 01:46:37 -0400

    starting some blog.rb work

--- routes/blog.rb
+++ routes/blog.rb
@@ -1,12 +1,18 @@
 class Minebound < Sinatra::Application
 
-  get '/write' do #TODO: a list of all the rounds you've been in 
+  get '/relate' do #TODO: a list of all the rounds you've been in 
   end
 
-  get '/write/:round' do #TODO: a textarea with the contents of your round story
+  get '/relate/:round' do #TODO: a textarea with the contents of your round story
   end
 
-  post '/write/:round' do #TODO: receive the most recent text and update it in the db
+  post '/relate/:round' do #TODO: receive the most recent text and update it in the db
+  end
+
+  get '/consider' do #TODO: show a complete list of past games
+  end
+
+  get '/consider/:game' do #TODO: show a game's journal
   end
 
 end
