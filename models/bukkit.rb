f20bee79f9449755756f9992d2840c063e163c48
Casey Kolderup <casey@kolderup.org>
2011-04-30 13:16:42 -0400

    use stop and go .sh when changing things on bukkit
    Also creating an ec2-scripts folder to store the shell scripts that live
    on ec2 that are used to manage the gnu screen session that contains the
    actual running bukkit process.

--- models/bukkit.rb
+++ models/bukkit.rb
@@ -2,26 +2,32 @@ class Bukkit
   
   def self.ban_user(mc_name)
     @commands = ""
+    @commands << "./stop.sh; "
     @commands << "rm bukkit/white-list.txt; "
     @commands << "echo \"#{ENV['ADMIN_MCNAME']}\" > bukkit/white-list.txt; "
     @commands << "echo \"#{mc_name}\" >> bukkit/banned-players.txt; "
+    @commands << "./go.sh; "
 
     ec2_ssh(@commands)
   end
 
   def self.reset_server
     @commands = ""
+    @commands << "./stop.sh; "
     @commands << "rm bukkit/white-list.txt; "
     @commands << "echo \"#{ENV['ADMIN_MCNAME']}\" > bukkit/white-list.txt; "
     @commands << "rm bukkit/banned-players.txt; "
     @commands << "rm bukkit/plugins/GhostBuster/ghosts.yml; "
+    @commands << "./go.sh; "
 
     ec2_ssh(@commands)
   end
 
   def self.white_list(mc_name)
     @commands = ""
-    @commands << "echo \"#{mc_name}\" >> bukkit/white-list.txt"
+    @commands << "./stop.sh; "
+    @commands << "echo \"#{mc_name}\" >> bukkit/white-list.txt; "
+    @commands << "./go.sh; "
 
     ec2_ssh(@commands)
   end
